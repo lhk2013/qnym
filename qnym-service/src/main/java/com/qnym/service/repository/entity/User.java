d720a5ffc730093972465a59cd3c7cc394fa8ff5
liuhaikuo <liuhaikuo@gomeplus.com>
2017-10-13 18:28:26 +0800

    增加主键配置

--- qnym-service/src/main/java/com/qnym/service/repository/entity/User.java
+++ qnym-service/src/main/java/com/qnym/service/repository/entity/User.java
@@ -2,22 +2,30 @@ package com.qnym.service.repository.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.Data;
 
 /**
  * Created by liuhaikuo on 2017/10/13.
  */
 @Entity
 @Table(name = "t_user")
+@Data
 public class User {
 
-    @Column(name = "")
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
     private String account;
 
     private String pwd;
 
+    @Column(name = "nick_name")
     private String nickName;
 
     private String phone;
