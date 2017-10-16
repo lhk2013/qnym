8c552564120e3ad9ecf87dd655149b0393dfc98c
liuhaikuo <liuhaikuo@gomeplus.com>
2017-10-16 16:57:37 +0800

    修改账户不存在的 msg

--- qnym-common/src/main/java/com/qnym/common/domain/LoginStatus.java
+++ qnym-common/src/main/java/com/qnym/common/domain/LoginStatus.java
@@ -7,7 +7,7 @@ package com.qnym.common.domain;
 public class LoginStatus {
     public final static HttpStatusNode SUCCESS = new HttpStatusNode(200, "ok");
     public final static HttpStatusNode ACCOUNT_EMPTY = new HttpStatusNode(40401, "account empty.");
-    public final static HttpStatusNode ACCOUNT_NOT_EXIST = new HttpStatusNode(40402, "pwd empty.");
+    public final static HttpStatusNode ACCOUNT_NOT_EXIST = new HttpStatusNode(40402, "account not exist.");
     public final static HttpStatusNode PWD_EMPTY = new HttpStatusNode(40403, "pwd empty.");
     public final static HttpStatusNode ACCOUNT_PWD_NOT_MATCH = new HttpStatusNode(40404, "account pwd not match.");
     public final static HttpStatusNode LOGIN_FAILD = new HttpStatusNode(40499, "account pwd not match.");
