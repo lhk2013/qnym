62f8a0c963b5b57d80fa9589e6348d7d64850123
liuhaikuo <liuhaikuo@gomeplus.com>
2017-10-20 10:20:47 +0800

    增加RSA 加解密工具类 包括公钥 密钥的生成规则

--- qnym-service/src/test/java/com/qnym/service/test/TestF.java
+++ qnym-service/src/test/java/com/qnym/service/test/TestF.java
@@ -1,11 +1,17 @@
 package com.qnym.service.test;
 
+import com.qnym.common.util.RSAUtils;
+import java.util.Map;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by liuhaikuo on 2017/10/16.
  */
 public class TestF {
+
+    private static Logger logger = LoggerFactory.getLogger(TestF.class);
 
     @Test
     public  void compare(){
@@ -23,6 +29,34 @@ public class TestF {
         if(d1 == d3){
             System.out.print(22222);
         }
+    }
+
+    @Test
+    public  void testRSA()throws Exception{
+        String accout = "lhk2013";
+        String pwd = "lhk1990211";
+        Map<String, byte[]> mp =  RSAUtils.initKey();
+        String pubKey = new String(RSAUtils.encryptBASE64(mp.get(RSAUtils.PUBLIC_KEY)));
+        String priKey = new String(RSAUtils.encryptBASE64(mp.get(RSAUtils.PRIVATE_KEY)));
+
+        logger.info("ff: {}",new String(mp.get(RSAUtils.PUBLIC_KEY)));
+        logger.info("public_key: {}",pubKey);
+        logger.info("private_key: {}",priKey);
+
+        byte[] rsaAcc = RSAUtils.encryptByPublicKey(accout.getBytes(),pubKey);
+        byte[] rsaPwd = RSAUtils.encryptByPublicKey(accout.getBytes(),pubKey);
+        String acc1 = RSAUtils.encryptBASE64(rsaAcc);
+        String pwd1 = RSAUtils.encryptBASE64(rsaPwd);
+        logger.info(acc1);
+        logger.info(pwd1);
+
+        byte[] deRsaAcc=RSAUtils.decryptByPrivateKey(RSAUtils.decryptBASE64(acc1),priKey);
+        byte[] deRsaPwd=RSAUtils.decryptByPrivateKey(RSAUtils.decryptBASE64(pwd1),priKey);
+
+        String acc2 = new String(deRsaAcc);
+        String pwd2 = new String(deRsaPwd);
+        logger.info(acc2);
+        logger.info(pwd2);
     }
 
 }
