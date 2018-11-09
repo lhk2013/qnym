2095af3002103ce2f709a0482a0d7e405d796f36
liuhaikuo <liuhaikuo@111.com.cn>
2018-11-09 11:37:30 +0800

    增加属性控制
    Signed-off-by: liuhaikuo <liuhaikuo@111.com.cn>

--- qnym-service/src/main/java/com/qnym/business/config/HttpEncodingAutoConfiguration.java
+++ qnym-service/src/main/java/com/qnym/business/config/HttpEncodingAutoConfiguration.java
@@ -13,14 +13,15 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 
 @Configuration   //表示这是一个配置类，以前编写的配置文件一样，也可以给容器中添加组件
-@EnableConfigurationProperties(HttpEncodingProperties.class)  //启动指定类的ConfigurationProperties功能；将配置文件中对应的值和HttpEncodingProperties绑定起来；并把HttpEncodingProperties加入到ioc容器中
-
-@ConditionalOnWebApplication//Spring底层@Conditional注解（Spring注解版），根据不同的条件，如果满足指定的条件，整个配置类里面的配置就会生效；    判断当前应用是否是web应用，如果是，当前配置类生效
-
+@EnableConfigurationProperties(HttpEncodingProperties.class)
+//启动指定类的ConfigurationProperties功能；将配置文件中对应的值和HttpEncodingProperties绑定起来；并把HttpEncodingProperties加入到ioc容器中
+@ConditionalOnWebApplication
+//Spring底层@Conditional注解（Spring注解版），根据不同的条件，如果满足指定的条件，整个配置类里面的配置就会生效；    判断当前应用是否是web应用，如果是，当前配置类生效
 @ConditionalOnClass(CharacterEncodingFilter.class)  //判断当前项目有没有这个类CharacterEncodingFilter；SpringMVC中进行乱码解决的过滤器；
 
-@ConditionalOnProperty(prefix = "spring.http.encoding", value = "enabled", matchIfMissing = true)  //判断配置文件中是否存在某个配置  spring.http.encoding.enabled；如果不存在，判断也是成立的
-//即使我们配置文件中不配置pring.http.encoding.enabled=true，也是默认生效的；
+
+@ConditionalOnProperty(prefix = "spring.http.encoding", value = "enabled", matchIfMissing = false)  //判断配置文件中是否存在某个配置  spring.http.encoding.enabled；如果不存在，判断不成立的
+//@ConditionalOnProperty(prefix = "spring.http.encoding", name = "enabled", havingValue = "true", matchIfMissing = false)
 public class HttpEncodingAutoConfiguration {
 
     //他已经和SpringBoot的配置文件映射了
@@ -36,8 +37,6 @@ public class HttpEncodingAutoConfiguration {
     public CharacterEncodingFilter characterEncodingFilter() {
         CharacterEncodingFilter filter = new OrderedCharacterEncodingFilter();
         filter.setEncoding(this.properties.getCharset().name());
-//        filter.setForceRequestEncoding(this.properties.shouldForce(Type.REQUEST));
-//        filter.setForceResponseEncoding(this.properties.shouldForce(Type.RESPONSE));
         return filter;
     }
 }
