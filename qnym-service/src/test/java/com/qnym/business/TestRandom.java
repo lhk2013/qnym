9a5899dd948cb2c4eb2503a7593ec0b6a42c2f4e
liuhaikuo <liuhaikuo@111.com.cn>
2019-01-30 10:36:23 +0800

    测试thrift 分支

--- qnym-service/src/test/java/com/qnym/business/TestRandom.java
+++ qnym-service/src/test/java/com/qnym/business/TestRandom.java
@@ -3,13 +3,30 @@ package com.qnym.business;
 import com.qnym.common.util.RandomUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
 
 @Slf4j
 public class TestRandom {
 
+    private static Logger logger = LoggerFactory.getLogger(TestRSA.class);
+
     @Test
     public void  testUniqueToken(){
+
+
         String token = RandomUtils.randomUniqueToken();
         log.info("token:{}",token);
+
+        ByteBuffer buffer_ = ByteBuffer.allocate(4);
+
+        int frameSize = buffer_.getInt(0);
+
+        System.out.println(        Integer.MIN_VALUE);
     }
+
+
+
 }
