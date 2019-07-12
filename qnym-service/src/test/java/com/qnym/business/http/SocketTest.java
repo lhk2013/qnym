9f7b34b6b265e00cdf49374a2f6a10b6a7510ced
liuhaikuo <liuhaikuo@111.com.cn>
2019-07-12 16:30:28 +0800

    增加Unicode解码

--- qnym-service/src/test/java/com/qnym/business/http/SocketTest.java
+++ qnym-service/src/test/java/com/qnym/business/http/SocketTest.java
@@ -1,5 +1,17 @@
 package com.qnym.business.http;
 
+import com.qnym.business.thread.ExcutorTest;
+
 public class SocketTest {
 
+
+    public static void main(String[] args) {
+        new ExcutorTest();
+        ExcutorTest ss = new ExcutorTest();
+        Object s =new ExcutorTest.TheadA();
+
+
+        System.out.println(s.toString());
+
+    }
 }
--- qnym-service/src/test/java/com/qnym/business/thread/ExcutorTest.java
+++ qnym-service/src/test/java/com/qnym/business/thread/ExcutorTest.java
@@ -1,8 +1,44 @@
 package com.qnym.business.thread;
 
+import javafx.concurrent.Task;
+
 import java.util.concurrent.*;
 
 public class ExcutorTest {
+
+
+    public static class TheadA implements Callable<String>{
+
+        @Override
+        public String call() throws Exception {
+            return "thead A";
+        }
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "TheadA{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    public static class TheadB implements Callable<String>{
+
+        @Override
+        public String call() throws Exception {
+            return "thead B";
+        }
+    }
 
 //    public static ExecutorService newSingleThreadExecutor() {
 //        return new FinalizableDelegatedExecutorServic (
@@ -16,7 +52,38 @@ public class ExcutorTest {
                 60L, TimeUnit.SECONDS,
                 new SynchronousQueue<Runnable>());
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 //        ThreadPoolExecutor
+//
+//        Task<String> task = new Task<String>() {
+//            @Override
+//            protected String call() throws Exception {
+//                return "fuck";
+//            }
+//        };
+
+//        String ss = task.get(5,TimeUnit.SECONDS);
+//        System.out.println(ss);
+
+        class Inner implements IA {
+            private int index;
+            @Override
+            public void fun() {
+                System.out.println("局部内部类的实例方法被调用,访问类中实例属性 index=" + index);
+            }
+            public Inner(int index) {
+                this.index = index;
+            }
+        }
+        Inner inner = new Inner(100);
     }
+
+
+}
+interface IA {
+    //接口中必须有你要从外界调用的方法；
+    void fun();
+}
+class ExcutorTest2 {
+
 }
